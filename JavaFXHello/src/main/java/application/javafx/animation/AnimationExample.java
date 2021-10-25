cb9b0e04d208b57a766319b793ada494b5f3f861
CISC181 <bgibbons@udel.edu>
2021-10-25 11:01:34 -0400

    Added examples

--- JavaFXHello/src/main/java/application/javafx/animation/AnimationExample.java
+++ JavaFXHello/src/main/java/application/javafx/animation/AnimationExample.java
@@ -1,20 +1,26 @@
-package pkgMain;
+package application.javafx.animation;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+/**
+ * A simple JavaFX animation examples. Animates a Circle's X property by translating (moving) it 200 points
+ * over 10 seconds.
+ */
 
-public class HelloFX extends Application {
+public class AnimationExample extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     public void start(Stage primaryStage) {
 
@@ -33,21 +39,4 @@ public class HelloFX extends Application {
 
         timeline.play();
     }
-    
-//    @Override
-//    public void start(Stage stage) {
-//         String javaVersion = System.getProperty("java.version");
-//         String javafxVersion = System.getProperty("javafx.version");
-//         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-//         
-// 
-//    	 Scene scene = new Scene(new StackPane(l), 640, 480);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
 }
--- JavaFXHello/src/main/java/module-info.java
+++ JavaFXHello/src/main/java/module-info.java
@@ -2,9 +2,76 @@ module JavaFXHello {
 	
 	
 	exports pkgMain;
+	exports application;
+	exports application.javafx.accordion;
+	exports application.javafx.animation;
+	exports application.javafx.apps;
+	exports application.javafx.button;
+	exports application.javafx.canvas;
+	exports application.javafx.choicebox;
+	exports application.javafx.color;
+	exports application.javafx.colorpicker;
+	exports application.javafx.combobox;
+	exports application.javafx.concurrency;
+	exports application.javafx.contextmenu;
+	exports application.javafx.directorychooser;
+	exports application.javafx.draganddrop;
+	exports application.javafx.filechooser;
+	exports application.javafx.font;
+	exports application.javafx.fxml;
+	exports application.javafx.gfx2d;
+	exports application.javafx.gfx3d;
+	exports application.javafx.hyperlink;
+	exports application.javafx.imageview;
+	exports application.javafx.label;
+	exports application.javafx.layout;
+	exports application.javafx.media;
+	exports application.javafx.menubutton;
+	exports application.javafx.pagination;
+	exports application.javafx.progressbar;
+	exports application.javafx.properties;
+	exports application.javafx.scene;
+	exports application.javafx.scrollpane;
+	exports application.javafx.separator;
+	exports application.javafx.slider;
+	exports application.javafx.splitmenubutton;
+	exports application.javafx.splitpane;
+	exports application.javafx.stage;
+	exports application.javafx.tableview;
+	exports application.javafx.tabpane;
+	exports application.javafx.text;
+	exports application.javafx.textarea;
+	exports application.javafx.textfield;
+	exports application.javafx.titledpane;
+	exports application.javafx.togglebutton;
+	exports application.javafx.toolbar;
+	exports application.javafx.tooltip;
+	exports application.javafx.transformations;
+	exports application.javafx.treetableview;
+	exports application.javafx.treeview;
+	exports application.javafx.vbox;
+	
 
 	requires javafx.base;
 	requires javafx.controls;
 	requires javafx.graphics;
+	requires javafx.fxml;
+	requires javafx.media;
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
